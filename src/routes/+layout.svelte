16c867eda727958752f32a6d9fdef4aff37d316c
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-04-25 00:40:13 +0200

    Hover, animation & component - Completely change hover of link items - Make Contact button conditional - Fix bouncing height on navbar - Move Button to its own component - Switch script tag to ts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
-<script>
+<script lang="ts">
 	import "../app.css";
+	import Button from "$ui/Button.svelte";
 
 	let scrollY = 0;
+	$: showButton = scrollY >= 150;
 </script>
 
 <svelte:window bind:scrollY />
 
 <nav
-	class="flex items-center justify-center px-20 py-5 sticky top-0 w-full z-10 backdrop-blur-sm backdrop-saturate-150 transition-shadow duration-500"
+	class="flex items-center justify-center px-20 py-5 sticky top-0 w-full h-20 z-10 backdrop-blur-sm backdrop-saturate-150 transition-shadow duration-500"
 	class:shadow-navbar-bottom={scrollY > 0}
 >
 	<a href="/" class="mr-auto">
@@ -18,15 +20,27 @@
 			class="h-8 duration-300 hover:opacity-70"
 		/>
 	</a>
-	<div
-		class="flex items-center gap-10 child:transition-opacity child:duration-200 child-hover:opacity-50"
-	>
-		<a href=".">About Us</a>
-		<a href=".">Our Work</a>
-		<a href=".">Who we are</a>
-		<a href="." class="font-medium border-primary border-2 text-primary rounded-lg py-1 px-3">
-			Contact
-		</a>
+	<div class="flex items-center gap-10">
+		<div
+			class="flex items-center gap-10 ease-out duration-700
+			child:relative
+			child:after:absolute child:after:bg-primary child:after:h-1 child:after:w-0 child:after:left-0 child:after:-bottom-1.5 child:after:duration-300
+			child-hover:after:w-full"
+			class:-mr-40={!showButton}
+		>
+			<a href=".">About Us</a>
+			<a href=".">Our Work</a>
+			<a href=".">Who we are</a>
+		</div>
+		<span
+			class="transition-opacity"
+			class:opacity-0={!showButton}
+			class:duration-200={!showButton}
+			class:duration-1000={showButton}
+			class:pointer-events-none={!showButton}
+		>
+			<Button type="secondary">Contact Us</Button>
+		</span>
 	</div>
 </nav>
 
