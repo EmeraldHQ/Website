4e8c0598fc9829d65824a363671ec08874bae279
MinixBF <benj.ferre@hotmail.fr>
2023-05-08 09:33:07 +0200

    format prettier

--- src/lib/ts/scroll.ts
+++ src/lib/ts/scroll.ts
@@ -1,13 +1,23 @@
-const scrollMargin = 40;
-
-export function scrollTo(selector: string) {
+/**
+ * Scrolls to the specified element with a smooth animation, taking into account the height of the navbar if applicable.
+ *
+ * @function
+ * @name scrollTo
+ *
+ * @param {string} selector - The CSS selector of the element to scroll to.
+ *
+ * @returns {void}
+ */
+export function scrollTo(selector: string): void {
 	const element = document.querySelector(selector);
 	if (element) {
 		const navbar = document.getElementById("navbar");
 		if (navbar && selector.startsWith("#")) {
-			// Minus height of navbar to scroll position to top of element and 40px for padding
-			const scrollPosition =
-				element.getBoundingClientRect().top + window.scrollY - navbar.clientHeight - scrollMargin;
+			// The navbar is fixed, so we need to account for its height.
+			const navbarHeight =
+				navbar.getBoundingClientRect().top + navbar.getBoundingClientRect().height;
+			const scrollPosition = element.getBoundingClientRect().top - navbarHeight + window.scrollY;
+
 			window.scrollTo({ top: scrollPosition, behavior: "smooth" });
 		} else {
 			element.scrollIntoView({ behavior: "smooth" });
