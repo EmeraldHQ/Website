2d155b6fea414b0f546e87e2fe712358cd5522ab
WarningImHack3r <WarningImHack3r@users.noreply.github.com>
2023-05-15 18:11:43 +0000

    Fix lint

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -6,37 +6,44 @@ import fs from "fs";
  * @returns an array of routes and their last modified date
  */
 function getRoutes(path: string) {
-    const files = fs.readdirSync(path).filter((file) => !file.includes("[") && !file.includes("]")); // Filter out dynamic routes
-    const routes: { path: string; lastMod: string }[] = [];
-    for (const file of files) {
-        const filePath = `${path}/${file}`;
-        const stats = fs.statSync(filePath);
-        if (stats.isDirectory()) {
-            routes.push(...getRoutes(filePath));
-        } else if (file === "+page.svelte") {
-            const lastmod = stats.mtime.toISOString().split("T")[0];
-            const path = filePath.replace("src/routes", "").replace("/+page.svelte", "");
-            routes.push({ path, lastMod: lastmod });
-        }
-    }
-    return routes;
+	const files = fs.readdirSync(path).filter((file) => !file.includes("[") && !file.includes("]")); // Filter out dynamic routes
+	const routes: { path: string; lastMod: string }[] = [];
+	for (const file of files) {
+		const filePath = `${path}/${file}`;
+		const stats = fs.statSync(filePath);
+		if (stats.isDirectory()) {
+			routes.push(...getRoutes(filePath));
+		} else if (file === "+page.svelte") {
+			const lastmod = stats.mtime.toISOString().split("T")[0];
+			const path = filePath.replace("src/routes", "").replace("/+page.svelte", "");
+			routes.push({ path, lastMod: lastmod });
+		}
+	}
+	return routes;
 }
 
-const rootURL = "https://renewhq.studio"
+const rootURL = "https://renewhq.studio";
 const routes = getRoutes("src/routes").map((route) => {
-    return {
-        ...route,
-        // Set the priority of the homepage to 1. All other pages are 0.7, unless they have been modified in the last 7 days, in which case they are 0.8
-        priority: route.path === "" ? 1 : new Date(route.lastMod).getTime() > Date.now() - 7 * 24 * 60 * 60 * 1000 ? 0.8 : 0.7,
-        // Set the change frequency to weekly, unless the page has not been modified in the last month, in which case it is monthly
-        changeFreq: new Date(route.lastMod).getTime() > Date.now() - 30 * 24 * 60 * 60 * 1000 ? "weekly" : "monthly"
-    };
+	return {
+		...route,
+		// Set the priority of the homepage to 1. All other pages are 0.7, unless they have been modified in the last 7 days, in which case they are 0.8
+		priority:
+			route.path === ""
+				? 1
+				: new Date(route.lastMod).getTime() > Date.now() - 7 * 24 * 60 * 60 * 1000
+				? 0.8
+				: 0.7,
+		// Set the change frequency to weekly, unless the page has not been modified in the last month, in which case it is monthly
+		changeFreq:
+			new Date(route.lastMod).getTime() > Date.now() - 30 * 24 * 60 * 60 * 1000
+				? "weekly"
+				: "monthly"
+	};
 });
 
-
 export async function GET() {
-    return new Response(
-        `
+	return new Response(
+		`
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset
             xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
@@ -46,8 +53,9 @@ export async function GET() {
             xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
             xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
         >
-        ${routes.map((route) => {
-            return `
+        ${routes
+					.map((route) => {
+						return `
                     <url>
                         <loc>${rootURL}${route.path}</loc>
                         <lastmod>${route.lastMod}</lastmod>
@@ -55,12 +63,13 @@ export async function GET() {
                         <priority>${route.priority}</priority>
                     </url>
                     `.trim();
-        }).join("\n")}
+					})
+					.join("\n")}
         </urlset>`.trim(),
-        {
-            headers: {
-                "Content-Type": "application/xml"
-            }
-        }
-    );
+		{
+			headers: {
+				"Content-Type": "application/xml"
+			}
+		}
+	);
 }
