EmeraldHQ/Website

commit 047c06622aeeecd60bc2a43bcd2bf9ad644a6c59

Meta changes (#8)
Co-authored-by: WarningImHack3r <WarningImHack3r@users.noreply.github.com>
Co-authored-by: MinixBF <40363781+MinixBF@users.noreply.github.com>

src/lib/elements/Button.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	export let type: "primary" | "secondary" = "primary";
+	import { createEventDispatcher } from "svelte";
+
+	/**
+	 * Defines the style of the button. Defaults to `"primary"`.
+	 */
+	export let type: "primary" | "secondary" | "minimal" = "primary";
+
+	const dispatch = createEventDispatcher();
 </script>
 
 {#if type === "primary"}
@@ -11,7 +18,8 @@
 		inline-flex items-center gap-2
 		rounded-xl shadow-lg shadow-primary/25 transition-colors duration-300
 		hover:bg-inherit hover:border-dominant hover:text-dominant
-	  {$$props.class}"
+		{$$props.class}"
+		on:click={() => dispatch("click")}
 	>
 		<slot />
 	</button>
@@ -23,7 +31,18 @@
 		inline-flex items-center gap-2
 		rounded-xl shadow-lg shadow-primary/25 transition-colors duration-300
 		hover:border-primary hover:text-primary
-	  {$$props.class}"
+	  	{$$props.class}"
+		on:click={() => dispatch("click")}
+	>
+		<slot />
+	</button>
+{:else if type === "minimal"}
+	<button
+		class="font-normal text-base text-dominant
+		flex justify-center items-center gap-1
+		hover:text-primary
+	  	{$$props.class}"
+		on:click={() => dispatch("click")}
 	>
 		<slot />
 	</button>

src/routes/+layout.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
 	import "../app.css";
+	import { onDestroy } from "svelte";
+	import { page } from "$app/stores";
 	import { fade } from "svelte/transition";
 	import { ArrowUp, Bars3 } from "@inqling/svelte-icons/heroicon-24-solid";
 	import { Github } from "@inqling/svelte-icons/simple-icons";
-	import Button from "$ui/Button.svelte";
-	import RadioButtonsGroup from "$ui/RadioButtonsGroup.svelte";
-	import SlideOver from "$components/SlideOver.svelte";
+	import Button from "$elements/Button.svelte";
+	// import RadioButtonsGroup from "$elements/RadioButtonsGroup.svelte";
+	import SlideOver from "$shells/SlideOver.svelte";
 	import resolveConfig from "tailwindcss/resolveConfig";
 	import tailwindConfig from "../../tailwind.config";
 	import { scrollTo } from "$ts/scroll";
+
+	// Breadcrumb
+	let currentRoute: string[] = [];
+	const unsubscribe = page.subscribe((value) => {
+		currentRoute = value?.route.id?.split("/").filter(Boolean) ?? [];
+	});
 
 	// Tailwind
 	const fullTailwindConfig = resolveConfig(tailwindConfig);
@@ -17,9 +25,10 @@
 
 	// Config
 	const navbarItems = [
-		{ name: "About Us", href: "#process" },
-		{ name: "Our Work", href: "#def" },
-		{ name: "Who we are", href: "#abc" },
+		{ name: "Solutions", href: "#" }, // Dropdown: 5/6 solutions
+		{ name: "Method", href: "#method" },
+		{ name: "Technologies", href: "#technologies" },
+		{ name: "Company", href: "#" }, // Dropdown: Values, Who we are
 		{ name: "Contact Us", href: "." }
 	];
 	const footerItems = [
@@ -67,6 +76,9 @@
 	let scrollY = 0;
 	$: showButton = scrollY >= scrollDistanceContactButton;
 	let showSlideOver = false;
+
+	// Lifecycle
+	onDestroy(unsubscribe);
 </script>
 
 <!-- Binding for scroll-dependent elements -->
@@ -74,58 +86,90 @@
 
 <!-- Navbar -->
 <div class="top-0 flex justify-center sticky w-full z-10 pt-10">
-	<nav
-		id="navbar"
-		class="flex items-center justify-center w-full max-w-large-screen px-10 md:px-20 py-5 mx-2 sm:mx-5 md:mx-10 h-20 bg-black/60 rounded-full backdrop-blur-sm backdrop-saturate-150 transition-shadow duration-500"
-	>
-		<a
-			href="/"
-			class="mr-auto grid overflow-hidden child:col-start-1 child:col-end-1 child:row-start-1 child:row-end-1"
+	<div class="w-full max-w-large-screen child:backdrop-blur-sm child:backdrop-saturate-150">
+		<nav
+			class="flex items-center justify-center px-10 md:px-20 py-5 mx-2 sm:mx-5 md:mx-10 h-20 bg-black/60 rounded-full"
 		>
-			{#if scrollY >= scrollDistanceLogoSwitch || (innerWidth && innerWidth < tailwindXsScreen)}
-				<img
-					in:fade={{ delay: 250 }}
-					out:fade
-					src="/favicon.svg"
-					alt="Renew logo - small"
-					class="h-8 duration-300 hover:opacity-70"
-				/>
-			{:else}
-				<img
-					in:fade={{ delay: 250 }}
-					out:fade
-					src="/logo-dark.svg"
-					alt="Renew logo"
-					class="h-8 duration-300 hover:opacity-70"
-				/>
-			{/if}
-		</a>
-		<div class="flex items-center gap-5 sm:gap-10">
-			<div
-				class="flex items-center gap-10 ease-out duration-700 max-lg:hidden nav-items-container"
-				class:-mr-40={!showButton}
-			>
-				{#each navbarItems.filter((item) => item.href.startsWith("#")) as item}
-					<button on:click={() => scrollTo(item.href)}>
-						{item.name}
-					</button>
-				{/each}
+			<div class="mr-auto flex items-center gap-5">
+				<a
+					href="/"
+					class="grid overflow-hidden child:col-start-1 child:col-end-1 child:row-start-1 child:row-end-1"
+				>
+					{#if scrollY >= scrollDistanceLogoSwitch || (innerWidth && innerWidth < tailwindXsScreen)}
+						<img
+							in:fade={{ delay: 250 }}
+							out:fade
+							src="/favicon.svg"
+							alt="Renew logo - small"
+							width="32"
+							height="32"
+							class="h-8 transition-opacity duration-300 hover:opacity-70"
+						/>
+					{:else}
+						<img
+							in:fade={{ delay: 250 }}
+							out:fade
+							src="/logo-dark.svg"
+							alt="Renew logo"
+							width="174"
+							height="32"
+							class="h-8 transition-opacity duration-300 hover:opacity-70"
+						/>
+					{/if}
+				</a>
 			</div>
-			<span
-				id="contact-us"
-				class="transition-opacity max-xs:hidden"
-				class:opacity-0={!showButton}
-				class:duration-200={!showButton}
-				class:duration-1000={showButton}
-				class:pointer-events-none={!showButton}
+			<div class="flex items-center gap-5 sm:gap-10">
+				<div
+					class="hidden lg:flex items-center gap-10 nav-items-container"
+					class:-mr-40={!showButton}
+				>
+					{#each navbarItems.filter((item) => item.href.startsWith("#")) as item}
+						<button on:click={() => scrollTo(item.href)}>
+							{item.name}
+						</button>
+					{/each}
+				</div>
+				<span
+					id="contact-us"
+					class="transition-opacity max-xs:hidden"
+					class:opacity-0={!showButton}
+					class:duration-200={!showButton}
+					class:duration-1000={showButton}
+					class:pointer-events-none={!showButton}
+				>
+					<Button type="secondary">Contact Us</Button>
+				</span>
+				<button class="lg:hidden" aria-label="Menu" on:click={() => (showSlideOver = true)}>
+					<Bars3 class="w-8 h-8" />
+				</button>
+			</div>
+		</nav>
+		{#if currentRoute.length > 0}
+			<div
+				class="mx-12 sm:mx-16 md:mx-20 py-1 px-6 sm:px-8 md:px-12 bg-black/70 rounded-b-3xl text-lg"
 			>
-				<Button type="secondary">Contact Us</Button>
-			</span>
-			<button class="lg:hidden" on:click={() => (showSlideOver = true)}>
-				<Bars3 class="w-8 h-8" />
-			</button>
-		</div>
-	</nav>
+				<div class="flex flex-row-reverse whitespace-nowrap overflow-x-auto">
+					<div class="flex flex-row gap-1.5 mr-auto">
+						{#each currentRoute as route, i}
+							<span>/</span>
+							<span>
+								{#if i < currentRoute.length - 1}
+									<a
+										href="/{currentRoute.slice(0, i + 1).join('/')}"
+										class="text-dominant underline-offset-4 hover:underline"
+									>
+										{route}
+									</a>
+								{:else}
+									{route}
+								{/if}
+							</span>
+						{/each}
+					</div>
+				</div>
+			</div>
+		{/if}
+	</div>
 </div>
 
 <!-- Responsive slide-over -->
@@ -158,20 +202,20 @@
 			<img src="/logo-dark.svg" alt="Renew logo" width="174" height="32" />
 		</a>
 	{/if}
-	<div class="flex flex-wrap gap-10 justify-between mt-10 xl:mt-0">
+	<div class="flex flex-wrap md:justify-center lg:justify-between gap-x-20 gap-y-16 my-14 xl:my-0">
 		{#if innerWidth >= tailwindXlScreen}
 			<a href="/" class="h-8 transition-opacity duration-300 hover:opacity-70">
 				<img src="/logo-dark.svg" alt="Renew logo" width="174" height="32" />
 			</a>
 		{/if}
 		{#each footerItems as column}
-			<div class="min-w-fit sm:mx-auto my-5 sm:my-10 xl:my-0">
-				<h4 class="text-primary mb-5">{column.name}</h4>
+			<div class="min-w-fit">
+				<h3 class="text-primary mb-5">{column.name}</h3>
 				<div
 					class="flex flex-col gap-2 child:w-fit child-hover:underline child:underline-offset-4 child-hover:text-dominant"
 				>
-					{#each column.items as link}
-						<a href={link.href}>{link.name}</a>
+					{#each column.items as item}
+						<a href={item.href}>{item.name}</a>
 					{/each}
 				</div>
 			</div>
@@ -184,7 +228,12 @@
 			<div
 				class="mb-5 text-primary divide-x divide-gray-400 child:transition-opacity child:duration-300 child-hover:opacity-70"
 			>
-				<a href="https://github.com/RenewHQ/Website">
+				<a
+					href="https://github.com/RenewHQ/Website"
+					aria-label="Take a look at the source code of this website"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
 					<Github class="w-8 h-8" />
 				</a>
 			</div>
@@ -220,6 +269,7 @@
 				</div>
 			{/if}
 			<!-- TODO: Actually make it work -->
+			<!--
 			<RadioButtonsGroup
 				values={["FR", "EN"]}
 				defaultIndex={1}
@@ -230,11 +280,16 @@
 					console.log(selectionIndex);
 				}}
 			/>
+			-->
 		</div>
 	</div>
 </footer>
 
 <style lang="postcss">
+	:global(::selection) {
+		@apply bg-dominant/75 text-primary;
+	}
+
 	:global(body) {
 		@apply bg-primary text-primary;
 	}

.github/dependabot.yml
@@ -3,5 +3,6 @@ updates:
   # Check weekly for updates to npm dependencies
   - package-ecosystem: "npm"
     directory: "/"
+    target-branch: "main"
     schedule:
-      interval: "weekly"
+      interval: "daily"

.github/workflows/pages-deploy.yml
@@ -6,8 +6,6 @@ on:
     branches: [main]
   pull_request:
 
-  workflow_dispatch:
-
 # Sets permissions of the GITHUB_TOKEN to allow deployment to GitHub Pages
 permissions:
   contents: read
@@ -26,6 +24,9 @@ jobs:
     steps:
       - name: 📂 Checkout
         uses: actions/checkout@v3
+        with:
+          ref: ${{ github.head_ref }}
+          token: ${{ secrets.WORKFLOW_PAT }}
 
       - name: 🔧 Configure pages
         uses: actions/configure-pages@v3
@@ -33,7 +34,7 @@ jobs:
         with:
           static_site_generator: "sveltekit"
 
-      - name: 🧭 Setting up Node
+      - name: 🧭 Setup Node
         uses: actions/setup-node@v3
         with:
           node-version: latest
@@ -46,8 +47,7 @@ jobs:
       - name: 🗄️ Get pnpm store directory
         id: pnpm-cache
         shell: bash
-        run: |
-          echo "STORE_PATH=$(pnpm store path)" >> $GITHUB_OUTPUT
+        run: echo "STORE_PATH=$(pnpm store path)" >> $GITHUB_OUTPUT
 
       - name: 🔄 Setup pnpm cache
         uses: actions/cache@v3
@@ -57,16 +57,31 @@ jobs:
           restore-keys: |
             ${{ runner.os }}-pnpm-store-
 
-      - name: 📥 Installing NPM dependencies
-        run: pnpm i
+      - name: 📥 Install NPM dependencies
+        run: pnpm i --no-frozen-lockfile
 
-      - name: ✨ Checking Svelte format
+      - name: ✨ Check Svelte format
         run: pnpm check
 
-      - name: ✨ Checking lint
+      - name: ✨ Check style with Prettier & ESLint
+        id: lint-check
         run: pnpm lint:ci
 
-      - name: 🔨 Building repo
+      # These steps will only ever trigger on PRs because merge is not allowed on failure
+      - name: 🔧 Fix lint (if needed)
+        if: ${{ failure() && steps.lint-check.conclusion == 'failure' }}
+        run: |
+          git reset --hard
+          pnpm format
+
+      - name: 📤 Commit lint fixes (if needed)
+        if: ${{ failure() && steps.lint-check.conclusion == 'failure' }}
+        uses: stefanzweifel/git-auto-commit-action@v4
+        with:
+          commit_message: "Fix lint"
+
+      # These steps won't run on previous errors (by default)
+      - name: 🔨 Build repo
         run: pnpm build
 
       - name: 📤 Upload artifact
@@ -81,7 +96,29 @@ jobs:
     environment:
       name: github-pages
       url: ${{ steps.deployment.outputs.page_url }}
+    outputs:
+      page_url: ${{ steps.deployment.outputs.page_url }}
     steps:
-      - name: 🌍 Deploying Pages
+      - name: 🌍 Deploy Pages
         uses: actions/deploy-pages@v2
         id: deployment
+
+  perf-check:
+    runs-on: ubuntu-latest
+    needs: deploy
+    steps:
+      - name: 📂 Checkout
+        uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
+
+      - name: ⚓️ Unlighthouse check
+        run: |
+          npm add @unlighthouse/cli puppeteer
+          unlighthouse-ci --site ${{ needs.deploy.outputs.page_url }}
+
+      - name: 📤 Upload artifact
+        uses: actions/upload-artifact@v3
+        with:
+          name: unlighthouse-report
+          path: .unlighthouse/ci-report.json

src/lib/elements/RadioButtonsGroup.svelte
@@ -2,9 +2,19 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
 
+	/**
+	 * The content of each toggle. Likely a string or an icon.
+	 */
 	export let values: unknown[] = [];
+	/**
+	 * The index of the default toggle. Defaults to 0, set to 0 if the value is out of bounds of `values`.
+	 */
 	export let defaultIndex = 0;
-	let currentIndex: number = defaultIndex;
+	let currentIndex: number;
+	$: currentIndex = defaultIndex < values.length && defaultIndex >= 0 ? defaultIndex : 0;
+	/**
+	 * The description of the radio buttons group. Used for accessibility. Mandatory.
+	 */
 	export let description: string;
 
 	const dispatch = createEventDispatcher();
@@ -17,6 +27,9 @@
 >
 	{#each values as toggle, index}
 		<button
+			role="radio"
+			aria-checked={index === currentIndex}
+			tabindex="0"
 			class="text-center px-4 py-1 rounded-full"
 			class:bg-slate-500={index === currentIndex}
 			class:hover:bg-slate-600={index === currentIndex}

src/lib/shells/MagneticElement.svelte
@@ -8,7 +8,13 @@
 	const dispatch = createEventDispatcher();
 
 	// Configuration
+	/**
+	 * The trigger area for the magnetic effect. Defaults to 200.
+	 */
 	export let triggerArea = 200;
+	/**
+	 * The interpolation factor for the magnetic effect. Often a value between 0 and 1. Defaults to 0.8.
+	 */
 	export let interpolationFactor = 0.8;
 
 	// Magnetic Object

commit d1a95aa26be01d9e7a5158bc623789bab2a2c846

UI refinements - Improve styling for minimal Button for consistency (no change) - Add (back) option to pass classes to Section - Improve responsive for Section title - Fix navbar animation regression from 047c06622aeeecd60bc2a43bcd2bf9ad644a6c59

src/lib/elements/Button.svelte
@@ -25,7 +25,7 @@
 	</button>
 {:else if type === "minimal"}
 	<button
-		class="flex items-center justify-center gap-1 text-base font-normal text-dominant underline-offset-4 hover:underline {$$props.class}"
+		class="inline-flex items-center gap-1 text-base font-normal text-dominant underline-offset-4 hover:underline {$$props.class}"
 		on:click={() => dispatch("click")}
 	>
 		<slot />

src/lib/layouts/Section.svelte
@@ -1,6 +1,6 @@
-<section id={$$props.id} class="py-10 child:mx-10 md:child:mx-20">
+<section id={$$props.id} class="py-10 child:mx-10 md:child:mx-20 {$$props.class}">
 	{#if $$slots.title}
-		<h2 class="!ml-24 pb-10 text-4xl font-medium drop-shadow-lg">
+		<h2 class="!md:ml-24 pb-10 text-4xl font-medium drop-shadow-lg">
 			<slot name="title" />
 		</h2>
 	{/if}

src/routes/+layout.svelte
@@ -8,9 +8,9 @@
 	import Button from "$elements/Button.svelte";
 	// import RadioButtonsGroup from "$elements/RadioButtonsGroup.svelte";
 	import SlideOver from "$shells/SlideOver.svelte";
+	import { scrollTo } from "$ts/scroll";
 	import resolveConfig from "tailwindcss/resolveConfig";
 	import tailwindConfig from "../../tailwind.config";
-	import { scrollTo } from "$ts/scroll";
 
 	// Breadcrumb
 	let currentRoute: string[] = [];
@@ -95,7 +95,7 @@
 					href="/"
 					class="grid overflow-hidden child:col-start-1 child:col-end-1 child:row-start-1 child:row-end-1"
 				>
-					{#if scrollY >= scrollDistanceLogoSwitch || (innerWidth && innerWidth < tailwindXsScreen)}
+					{#if scrollY >= scrollDistanceLogoSwitch || (innerWidth > 0 && innerWidth < tailwindXsScreen)}
 						<img
 							in:fade={{ delay: 250 }}
 							out:fade
@@ -120,7 +120,7 @@
 			</div>
 			<div class="flex items-center gap-5 sm:gap-10">
 				<div
-					class="nav-items-container hidden items-center gap-10 lg:flex"
+					class="nav-items-container hidden items-center gap-10 duration-700 ease-out lg:flex"
 					class:-mr-40={!showButton}
 				>
 					{#each navbarItems.filter(item => item.href.startsWith("#")) as item}