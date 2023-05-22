e7112adfbfae75db119b95bf0582b988f4ced15a
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-05-22 16:55:27 +0200

    Set section title font to medium

--- src/lib/layouts/Section.svelte
+++ src/lib/layouts/Section.svelte
@@ -1,6 +1,6 @@
 <section id={$$props.id} class="py-10 child:mx-10 md:child:mx-20">
 	{#if $$slots.title}
-		<h2 class="!ml-24 pb-10 text-4xl drop-shadow-lg">
+		<h2 class="!ml-24 pb-10 text-4xl font-medium drop-shadow-lg">
 			<slot name="title" />
 		</h2>
 	{/if}
