20e713d155d3bc3dec06b4eaeda15065088823be
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2023-12-14 20:37:19 +0100

    Migrate to SvelteKit 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import * as path from "path";
 
 /** @type {import("@sveltejs/kit").Config} */
