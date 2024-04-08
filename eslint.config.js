1c8e7aac141062a09bdddc77e75208f6bc32e5f5
Antoine Lethimonnier <43064022+WarningImHack3r@users.noreply.github.com>
2024-04-08 10:48:24 +0200

    Update eslint.config.js
    From https://github.com/sveltejs/kit/pull/12089/files#diff-f34875eadf2608fbb73b98dda130f47f65e2f79c8dde682c9b7f4ad6f0cb2735

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,30 @@
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import globals from "globals";
-import svelteEslintParser from "svelte-eslint-parser";
-import eslintPluginSvelte from "eslint-plugin-svelte";
+import ts from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
 import prettier from "eslint-plugin-prettier/recommended";
+import globals from "globals";
 
-/** @type {import("eslint").Linter.FlatConfig} */
+/** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
-	...tseslint.config({
-		plugins: {
-			"@typescript-eslint": tseslint.plugin
-		},
-		languageOptions: {
-			parser: tseslint.parser,
-			parserOptions: {
-				sourceType: "module",
-				ecmaVersion: 2020,
-				extraFileExtensions: [".svelte"]
-			},
-			globals: {
-				...globals.browser
-			}
-		},
-		rules: {
-			// Prefer `{ myFunction: () => {} }` over `{ myFunction() {} }`
-			// https://www.totaltypescript.com/method-shorthand-syntax-considered-harmful
-			"@typescript-eslint/method-signature-style": ["error", "property"],
-			// Force the use of `import type { A }` over `import { type A }`
-			// https://typescript-eslint.io/rules/no-import-type-side-effects/
-			"@typescript-eslint/no-import-type-side-effects": "error"
-		}
-	}),
+	...ts.configs.recommended,
+	...svelte.configs["flat/recommended"],
+	prettier,
 	{
-		files: ["*.svelte"],
 		languageOptions: {
-			parser: svelteEslintParser,
+			ecmaVersion: 2020,
+			globals: {
+				...globals.browser,
+				...globals.es2017,
+				...globals.node
+			}
+		}
+	},
+	{
+		files: ["**/*.svelte"],
+		languageOptions: {
 			parserOptions: {
-				parser: tseslint.parser
+				parser: ts.parser
 			}
 		},
 		rules: {
@@ -46,22 +33,23 @@ export default [
 				{
 					varsIgnorePattern: "^\\$\\$(Props|Events|Slots)$"
 				}
-			]
+			],
+			// Prefer `{ myFunction: () => {} }` over `{ myFunction() {} }`
+			// https://www.totaltypescript.com/method-shorthand-syntax-considered-harmful
+			"@typescript-eslint/method-signature-style": ["error", "property"],
+			// Force the use of `import type { A }` over `import { type A }`
+			// https://typescript-eslint.io/rules/no-import-type-side-effects/
+			"@typescript-eslint/no-import-type-side-effects": "error"
 		}
 	},
-	...eslintPluginSvelte.configs["flat/recommended"],
-	prettier,
 	{
 		ignores: [
 			".DS_Store",
-			"node_modules/*",
-			"build/*",
-			".svelte-kit/*",
-			"src/paraglide/*",
-			"package/*",
-			".env",
-			".env.*",
-			"pnpm-lock.yaml"
+			"build/",
+			".svelte-kit/",
+			"package/",
+			"vite.config.ts.*",
+			"src/paraglide/"
 		]
 	}
 ];
