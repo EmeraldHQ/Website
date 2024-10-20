02af0404427d63b7bf35162ecc2b8ec299b40ffd
WarningImHack3r <43064022+WarningImHack3r@users.noreply.github.com>
2024-10-20 22:55:51 +0200

    Uneasily swap drawer, but the result is worth it

--- src/lib/shells/SlideOver.svelte
+++ src/lib/shells/SlideOver.svelte
@@ -3,13 +3,7 @@
 	import { writable } from "svelte/store";
 	import * as m from "$paraglide/messages";
 	import XMark from "@inqling/svelte-icons/heroicon-24-outline/x-mark.svelte";
-	import {
-		Dialog,
-		DialogOverlay,
-		DialogTitle,
-		Transition,
-		TransitionChild
-	} from "@rgossiaux/svelte-headlessui";
+	import { Drawer } from "vaul-svelte";
 
 	type Props = {
 		/**
@@ -27,12 +21,66 @@
 	let { show = $bindable(false), title = undefined, children, footer }: Props = $props();
 
 	/**
+	 * This bit of logic is there because vaul-svelte has no animation when controlled programmatically:
+	 * https://github.com/huntabyte/vaul-svelte/issues/52
+	 *
+	 * To work around this, we create a proxy around the `show` prop to trigger a close animation when
+	 * the prop is set to `false`.
+	 */
+	let isOpen = $state(show);
+	let closer = $state<HTMLSpanElement>();
+
+	$effect(() => {
+		if (show) {
+			isOpen = true;
+		} else {
+			closer?.click();
+		}
+	});
+
+	/**
 	 * A store containing a callback function to call when the slideover closed.
 	 */
 	const onCloseStore = writable(() => {});
 </script>
 
-<Transition {show}>
+<Drawer.Root
+	direction="right"
+	bind:open={isOpen}
+	onOpenChange={isOpen => {
+		if (isOpen) return; // this seems unnecessary because it seems to never be called with `true`
+		setTimeout(() => {
+			show = false;
+			$onCloseStore();
+			onCloseStore.set(() => {});
+		}, 200); // required delay to fully close, otherwise the scroll lock is still active
+	}}
+>
+	<Drawer.Portal>
+		<Drawer.Overlay class="fixed inset-0 z-50 bg-black/80" />
+		<Drawer.Content
+			class="fixed inset-y-0 right-0 z-50 flex h-auto w-screen max-w-md flex-col bg-secondary"
+		>
+			<Drawer.Close class="ml-auto mr-5 mt-5 p-2 transition-opacity hover:opacity-75">
+				<span class="sr-only" bind:this={closer}>{m.a11yAriaPanelClose()}</span>
+				<XMark class="size-6" aria-hidden="true" />
+			</Drawer.Close>
+			{#if title}
+				<Drawer.Title class="text-lg font-medium">
+					{title}
+				</Drawer.Title>
+			{/if}
+			{@render children?.({ onClose: onCloseStore })}
+			{#if footer}
+				<div class="border-t border-primary px-4 py-6 sm:px-6">
+					{@render footer?.()}
+				</div>
+			{/if}
+		</Drawer.Content>
+	</Drawer.Portal>
+</Drawer.Root>
+
+<!--<Transition {show}>
 	<Dialog class="relative z-10" on:close={() => (show = false)}>
 		<TransitionChild
 			enter="ease-in-out duration-500"
@@ -93,4 +141,4 @@
 			</div>
 		</div>
 	</Dialog>
-</Transition>
+</Transition>-->
